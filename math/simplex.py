29aa8ec7615d5bb0923628052db5e4d2a30f4c73
Josef Podaný <podany.ml@onio.cz>
2019-01-07 13:07:02 +0100

    NEW: .gitignore

---
+++ math/simplex.py
@@ -0,0 +1,16 @@
+''' simplex calculation '''
+
+
+'''
+header  x1  x2  x3  ... xn  01  02  03  ... 0n  result
+row_1   r11 r12 r13 ... r1n 1   0   0   ... 0   restriction_1
+row_2   r21 r22 r23 ... r2n 0   1   0   ... 0   restricion_2
+row_n   rn1 rn2 rn3 ... rnn 0   0   0   ... 1   restriction_n
+'''
+
+
+def simplex(func, restricitons):
+    '''
+    Calculates optimal result with regards to restrictions
+    '''
+    pass
