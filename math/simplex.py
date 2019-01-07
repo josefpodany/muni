bfab045b620a1a313e9c3388948614a9a6bde73c
Josef Podaný <podany.ml@onio.cz>
2019-01-07 13:39:24 +0100

    ADD: computing slack variables

--- math/simplex.py
+++ math/simplex.py
@@ -9,11 +9,34 @@ row_n   rn1 rn2 rn3 ... rnn 0   0   0   ... 1   restriction_n
 '''
 
 
-def simplex(func, restricitons):
+def simplex_method(func, restr):
     '''
     Calculates optimal result with regards to restrictions
     :param func: list of variables of maximaliazation function
-    :param restrictions: list of restrictions of variables without slack variables
-    :return: tuple with solved coefficients and optimal maximum value of function
+    :param restr: 2D list of restrictions of variables without slack 
+    variables
+    :return: tuple with solved coefficients and optimal maximum value of 
+    function
+
+    example:
+    func: [3, 1, 2, 2]
+    restrictions: [[1, 2, 0, 0, 300], [2, 3, 1, 1, 500]]
     '''
-    pass
+    if all([len(i) == len(func) + 1 for i in restr]):
+        function = func[:]
+        restrictions = [row[:] for row in restr]
+        # Create slack variables in header
+        function += (len(restrictions) + 1) * [0]
+        # Create slack variables in the restrictions
+        for i in range(len(restrictions)):
+            total = restrictions[i].pop()
+            for j in range(len(restrictions[i])):
+                restrictions[i] += [1] if j == i else [0]
+            restrictions[i].append(total)
+    else:
+        raise ValueError("Restrictions of simplex method do not have larger\
+ amount of variables than maximaliazation function by one")
+
+
+if __name__ == "__main__":
+    simplex_method([1, 2], [[1, 2, 3], [1, 2, 3]])
