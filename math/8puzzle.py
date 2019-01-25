747c0248666de60dfe8f1b8efbae6d57884ff6b2
Josef Podaný <podany.ml@onio.cz>
2019-01-25 13:59:01 +0100

    FIX: comments

--- math/8puzzle.py
+++ math/8puzzle.py
@@ -1,6 +1,3 @@
-#!/usr/local/bin/python
-__author__ = "Josef Podany, V4D PROG"
-__email__ = "josef.podany.ml@gmail.com"
 import time
 # 1. check all valid moves
 # 2. create all possible children (check all ancestors)
@@ -9,8 +6,9 @@ import time
 # 5. check if children in range are solution
 # 6. again
 
+
 class Puzzle:
-    def __init__(self, values, ancestors = [], moves = []):
+    def __init__(self, values, ancestors=[], moves=[]):
         # takes in array of values
         self.values = values
         self.ancestors = ancestors      # array of indeces of all ancestors
@@ -21,16 +19,19 @@ class Puzzle:
         }
 
     def __str__(self):
-        string =  "layer       : " + str(len(self.ancestors)) + "\n"
+        string = "layer       : " + str(len(self.ancestors)) + "\n"
         string += "ancestors   : " + str(self.ancestors) + "\n"
         string += "moves       : "
         for move in self.moves:
-            string += self.possible_moves["words"][self.possible_moves["shifts"].index(move)] + " "
+            string += self.possible_moves["words"][self.possible_moves["shifts"].index(
+                move)] + " "
         string += "\n"
         for value in self.values:
-            string = string + str(value) + "\n" if self.values.index(value) % 3 == 2 else string + str(value) + " "
+            string = string + \
+                str(value) + "\n" if self.values.index(value) % 3 == 2 else string + \
+                str(value) + " "
         return string
-    
+
     def get_values(self):
         return self.values
 
@@ -48,7 +49,8 @@ class Puzzle:
             new_index = shift + position_of_zero
             if 0 <= new_index < len(self.values):
                 if not ((position_of_zero in [3, 6] and shift == -1) or (position_of_zero in [2, 5] and shift == 1)):
-                    temp_values = self.values[:]        # deep copy to create copy and not link
+                    # deep copy to create copy and not link
+                    temp_values = self.values[:]
                     temp_values[new_index], temp_values[position_of_zero] = temp_values[position_of_zero], temp_values[new_index]
                     valid_states.append({
                                         "array": temp_values,
@@ -57,7 +59,7 @@ class Puzzle:
 
 
 class Solver:
-    def __init__(self, root, goal, max_layers = 25, time_elapsed = 0):
+    def __init__(self, root, goal, max_layers=25, time_elapsed=0):
         self.tree = [root]
         self.goal = goal
         self.max_layers = max_layers
@@ -70,12 +72,14 @@ class Solver:
         indeces_of_ancestors = node.get_indeces_of_ancestors()
         if indeces_of_ancestors is not None:
             ancestors = self.get_ancestors(indeces_of_ancestors)
-            ancestors_values = [ancestor.get_values() for ancestor in ancestors]
+            ancestors_values = [ancestor.get_values()
+                                for ancestor in ancestors]
             for state in states:
                 if state["array"] not in ancestors_values:
-                    new_child = Puzzle(state["array"], indeces_of_ancestors + [parent_node_index], node.get_moves() + [state["direction"]])
+                    new_child = Puzzle(state["array"], indeces_of_ancestors + [
+                                       parent_node_index], node.get_moves() + [state["direction"]])
                     self.tree.append(new_child)
-    
+
     def show_solution(self, node):
         for index in node.ancestors:
             print(self.tree[index])
@@ -88,7 +92,8 @@ class Solver:
         start = time.time()
         while True:
             temp = len(self.tree)
-            for index in range(left, len(self.tree)):    # checks the solution and adds a new layer of nodes
+            # checks the solution and adds a new layer of nodes
+            for index in range(left, len(self.tree)):
                 node = self.tree[index]
                 if node.get_indeces_of_ancestors() is not None:
                     if len(node.get_indeces_of_ancestors()) > self.max_layers:
@@ -104,6 +109,7 @@ class Solver:
                     self.create_children_of_node(node, index)
             left = temp
 
+
 if __name__ == '__main__':
     root1 = Puzzle([0, 1, 3, 4, 2, 5, 7, 8, 6])
     root2 = Puzzle([1, 2, 3, 0, 5, 6, 4, 7, 8])
@@ -112,7 +118,3 @@ if __name__ == '__main__':
     solver = Solver(root3, goal)
     if solver.solve():
         solver.show_solution(solver.solve())
-        
-
-
-
